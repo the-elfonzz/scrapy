901d3aeca8c8e3a4e58922ca0b5681b105b88bcb
Alexander Kostov <akexanderko@aiidatapro.com>
2017-02-06 22:31:26 +0200

    To Spider arguments

--- sasho_spider/tutorial/spiders/quotes_spider.py
+++ sasho_spider/tutorial/spiders/quotes_spider.py
@@ -3,19 +3,37 @@ import scrapy
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/'
+    start_urls = [
+        'http://quotes.toscrape.com/',
+#        'http://quotes.toscrape.com/page/2/'
         ]
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self. response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Save file %s' % filename)
+    # def start_requests(self):
+    #     urls =
+    #
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Save file %s' % filename)
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract_first(),
+            }
+        next_page = response.xpath("//ul[@class='pager']//a/@href").extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    # def parse(self, response):
+    #     print '###############################################################'
+    #     self.logger.info('Response from %s arrived!', response.url)
