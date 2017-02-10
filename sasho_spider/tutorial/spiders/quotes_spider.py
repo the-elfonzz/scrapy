1281bab407bf6182d3d35854657c7cc72d7fb4c0
Alexander Kostov <akexanderko@aiidatapro.com>
2017-02-10 19:38:49 +0200

    Item Pipeline

--- sasho_spider/tutorial/spiders/quotes_spider.py
+++ sasho_spider/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+from tutorial.items import *
+from scrapy.loader import ItemLoader
+
 
 
 class QuoteSpider(scrapy.Spider):
@@ -22,21 +25,28 @@ class QuoteSpider(scrapy.Spider):
     #         f.write(response.body)
     #     self.log('Save file %s' % filename)
 
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('span small::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract_first(),
-            }
-        next_page = response.xpath("//ul[@class='pager']//a/@href").extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-
     # def parse(self, response):
-    #     print '###############################################################'
     #     self.logger.info('Response from %s arrived!', response.url)
+    #     for quote in response.css('div.quote'):
+    #         yield {
+    #             'text': quote.css('span.text::text').extract_first(),
+    #             'author': quote.css('span small::text').extract_first(),
+    #             'tags': quote.css('div.tags a.tag::text').extract_first(),
+    #         }
+    #     next_page = response.xpath("//ul[@class='pager']//a/@href").extract_first()
+    #     if next_page is not None:
+    #         next_page = response.urljoin(next_page)
+    #         yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse(self, response):
+        l = ItemLoader(item=Product(), response=response)
+        l.add_xpath('name', '//div[@class="product_name"]')
+        l.add_xpath('name', '//div[@class="product_title"]')
+        l.add_xpath('price', '//p[@id="price"]')
+        l.add_css('stocl', 'p#stock')
+        l.add_value('last_updated', 'today')
+        print l.load_item()
+        return l.load_item()
 
     def parse_item(self, response):
         self.logger.info("Response page %s", response.url)
